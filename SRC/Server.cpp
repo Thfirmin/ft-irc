f9f887e5feb3030d6656ab341de6a0a945be25a5
Thfirmin <thfirmin.dsantos+github@gmail.com>
2024-08-11 21:49:44 -0300

    update

--- SRC/Server.cpp
+++ SRC/Server.cpp
@@ -1,42 +1,13 @@
 #include "../INC/Server.hpp"
 
-/**
- * Indica se o servidor está cheio de bots.
- * 
- * Esta variável é usada para monitorar se o servidor atingiu a capacidade máxima de bots.
- */
 bool Server::isBotfull = false;
 
-/**
- * Construtor da classe Server.
- * 
- * Inicializa a instância do servidor com o descritor de socket definido como -1.
- */
 Server::Server(){this->server_fdsocket = -1;}
 
-/**
- * Destrutor da classe Server.
- * 
- * Realiza a limpeza necessária quando um objeto Server é destruído.
- */
 Server::~Server(){}
 
-/**
- * Construtor de cópia da classe Server.
- * 
- * @param src Objeto Server a ser copiado.
- * Copia os valores do objeto src para o objeto atual.
- */
 Server::Server(Server const &src){*this = src;}
 
-/**
- * Operador de atribuição para a classe Server.
- * 
- * @param src Objeto Server a ser copiado.
- * @return Referência para o objeto Server atual.
- * 
- * Realiza a cópia dos atributos do objeto src para o objeto atual, evitando auto-atribuição.
- */
 Server &Server::operator=(Server const &src){
 	if (this != &src){
 		/*
@@ -55,26 +26,10 @@ Server &Server::operator=(Server const &src){
 	return *this;
 }
 
-/**
- * Obtém o número da porta do servidor.
- * 
- * @return O número da porta do servidor.
- */
 int Server::GetPort(){return this->port;}
 
-/**
- * Obtém o descritor de socket do servidor.
- * 
- * @return O descritor de socket do servidor.
- */
 int Server::GetFd(){return this->server_fdsocket;}
 
-/**
- * Obtém um cliente pelo descritor de arquivo.
- * 
- * @param fd Descritor de arquivo do cliente.
- * @return Ponteiro para o objeto Client correspondente ao descritor de arquivo, ou NULL se não encontrado.
- */
 Client *Server::GetClient(int fd){
 	for (size_t i = 0; i < this->clients.size(); i++){
 		if (this->clients[i].GetFd() == fd)
@@ -83,12 +38,6 @@ Client *Server::GetClient(int fd){
 	return NULL;
 }
 
-/**
- * Obtém um cliente pelo apelido (nickname).
- * 
- * @param nickname O apelido do cliente.
- * @return Ponteiro para o objeto Client correspondente ao apelido, ou NULL se não encontrado.
- */
 Client *Server::GetClientNick(std::string nickname){
 	for (size_t i = 0; i < this->clients.size(); i++){
 		if (this->clients[i].GetNickName() == nickname)
@@ -97,12 +46,6 @@ Client *Server::GetClientNick(std::string nickname){
 	return NULL;
 }
 
-/**
- * Obtém um canal pelo nome.
- * 
- * @param name O nome do canal.
- * @return Ponteiro para o objeto Channel correspondente ao nome, ou NULL se não encontrado.
- */
 Channel *Server::GetChannel(std::string name)
 {
 	for (size_t i = 0; i < this->channels.size(); i++){
@@ -112,60 +55,20 @@ Channel *Server::GetChannel(std::string name)
 	return NULL;
 }
 
-/**
- * Define o descritor de socket do servidor.
- * 
- * @param fd O novo descritor de socket.
- */
 void Server::SetFd(int fd){this->server_fdsocket = fd;}
 
-/**
- * Define o número da porta do servidor.
- * 
- * @param port O novo número da porta.
- */
 void Server::SetPort(int port){this->port = port;}
 
-/**
- * Define a senha do servidor.
- * 
- * @param password A nova senha.
- */
 void Server::SetPassword(std::string password){this->password = password;}
 
-/**
- * Define a senha do servidor.
- * 
- * @param password A nova senha.
- */
 std::string Server::GetPassword(){return this->password;}
 
-/**
- * Adiciona um novo cliente à lista de clientes do servidor.
- * 
- * @param newClient O novo cliente a ser adicionado.
- */
 void Server::AddClient(Client newClient){this->clients.push_back(newClient);}
 
-/**
- * Adiciona um novo canal à lista de canais do servidor.
- * 
- * @param newChannel O novo canal a ser adicionado.
- */
 void Server::AddChannel(Channel newChannel){this->channels.push_back(newChannel);}
 
-/**
- * Adiciona um novo descritor de arquivo à lista de descritores monitorados pelo servidor.
- * 
- * @param newFd O novo descritor de arquivo a ser adicionado.
- */
 void Server::AddFds(pollfd newFd){this->fds.push_back(newFd);}
 
-/**
- * Remove um cliente da lista de clientes do servidor pelo descritor de arquivo.
- * 
- * @param fd O descritor de arquivo do cliente a ser removido.
- */
 void Server::RemoveClient(int fd){
 	for (size_t i = 0; i < this->clients.size(); i++){
 		if (this->clients[i].GetFd() == fd)
@@ -173,11 +76,6 @@ void Server::RemoveClient(int fd){
 	}
 }
 
-/**
- * Remove um canal da lista de canais do servidor pelo nome.
- * 
- * @param name O nome do canal a ser removido.
- */
 void Server::RemoveChannel(std::string name){
 	for (size_t i = 0; i < this->channels.size(); i++){
 		if (this->channels[i].GetName() == name)
@@ -185,11 +83,6 @@ void Server::RemoveChannel(std::string name){
 	}
 }
 
-/**
- * Remove um descritor de arquivo da lista de descritores monitorados pelo servidor.
- * 
- * @param fd O descritor de arquivo a ser removido.
- */
 void Server::RemoveFds(int fd){
 	for (size_t i = 0; i < this->fds.size(); i++){
 		if (this->fds[i].fd == fd)
@@ -197,11 +90,6 @@ void Server::RemoveFds(int fd){
 	}
 }
 
-/**
- * Remove um cliente de todos os canais em que está presente.
- * 
- * @param fd O descritor de arquivo do cliente a ser removido dos canais.
- */
 void	Server::RmChannels(int fd){
 	for (size_t i = 0; i < this->channels.size(); i++){
 		int flag = 0;
@@ -218,14 +106,6 @@ void	Server::RmChannels(int fd){
 	}
 }
 
-/**
- * Envia uma mensagem de erro para um cliente específico.
- * 
- * @param code Código de erro.
- * @param clientname Nome do cliente.
- * @param fd Descritor de arquivo do cliente.
- * @param msg Mensagem de erro adicional.
- */
 void Server::senderror(int code, std::string clientname, int fd, std::string msg)
 {
 	std::stringstream ss;
@@ -235,15 +115,6 @@ void Server::senderror(int code, std::string clientname, int fd, std::string msg
 		std::cerr << "send() faild" << std::endl;
 }
 
-/**
- * Envia uma mensagem de erro para um cliente específico dentro de um canal.
- * 
- * @param code Código de erro.
- * @param clientname Nome do cliente.
- * @param channelname Nome do canal.
- * @param fd Descritor de arquivo do cliente.
- * @param msg Mensagem de erro adicional.
- */
 void Server::senderror(int code, std::string clientname, std::string channelname, int fd, std::string msg)
 {
 	std::stringstream ss;
@@ -253,32 +124,14 @@ void Server::senderror(int code, std::string clientname, std::string channelname
 		std::cerr << "send() faild" << std::endl;
 }
 
-/**
- * Envia uma resposta para um cliente específico.
- * 
- * @param response A resposta a ser enviada.
- * @param fd Descritor de arquivo do cliente.
- */
 void Server::_sendResponse(std::string response, int fd)
 {
 	if(send(fd, response.c_str(), response.size(), 0) == -1)
 		std::cerr << "Response send() faild" << std::endl;
 }
 
-/**
- * Indica se um sinal foi recebido.
- * 
- * Esta variável é usada para monitorar se o servidor recebeu um sinal.
- */
 bool Server::Signal = false;
 
-/**
- * Manipulador de sinais para o servidor.
- * 
- * @param signum O número do sinal recebido.
- * 
- * Define a variável Server::Signal como true quando um sinal é recebido e exibe uma mensagem.
- */
 void Server::SignalHandler(int signum)
 {
 	(void)signum;
@@ -286,11 +139,6 @@ void Server::SignalHandler(int signum)
 	Server::Signal = true;
 }
 
-/**
- * Fecha todos os descritores de arquivo associados aos clientes e ao servidor.
- * 
- * Exibe uma mensagem indicando o fechamento de cada cliente e do servidor.
- */
 void	Server::closeFds(){
 	for(size_t i = 0; i < clients.size(); i++){
 		std::cout << RED << "Client <" << clients[i].GetFd() << "> Disconnected" << WHI << std::endl;
@@ -302,17 +150,6 @@ void	Server::closeFds(){
 	}
 }
 
-/**
- * Inicializa o servidor com a porta e senha especificadas.
- * 
- * @param port O número da porta para o servidor.
- * @param pass A senha do servidor.
- * 
- * Configura o socket do servidor e aguarda novas conexões. Em um loop, utiliza `poll` para monitorar eventos nos descritores
- * de arquivo. Aceita novos clientes ou recebe dados dos clientes existentes, conforme necessário.
- * 
- * @throws std::runtime_error Se ocorrer algum erro durante a configuração do socket ou na chamada a `poll`.
- */
 void Server::init(int port, std::string pass)
 {
 	this->password = pass;
@@ -338,14 +175,6 @@ void Server::init(int port, std::string pass)
 	closeFds();
 }
 
-/**
- * Configura o socket do servidor.
- * 
- * Configura o socket do servidor, define opções de socket, torna o socket não bloqueante, associa o socket ao endereço e 
- * inicia a escuta de conexões.
- * 
- * @throws std::runtime_error Se ocorrer algum erro na criação do socket, definição de opções, associação ou escuta.
- */
 void Server::setSeverSocket()
 {
 	int en = 1;
@@ -369,13 +198,6 @@ void Server::setSeverSocket()
 	fds.push_back(new_cli);
 }
 
-/**
- * Aceita uma nova conexão de cliente.
- * 
- * Aceita um novo cliente que está tentando se conectar ao servidor. Define o descritor de arquivo do cliente,
- * torna o descritor de arquivo não bloqueante, armazena o cliente na lista de clientes e adiciona o descritor
- * de arquivo do cliente na lista de descritores monitorados.
- */
 void Server::acceptNewClient()
 {
 	Client cli;
@@ -396,15 +218,6 @@ void Server::acceptNewClient()
 	std::cout << GRE << "Client <" << incofd << "> Connected" << WHI << std::endl;
 }
 
-/**
- * Recebe novos dados de um cliente.
- * 
- * @param fd O descritor de arquivo do cliente.
- * 
- * Recebe dados do cliente associado ao descritor de arquivo especificado. Os dados são armazenados
- * no buffer do cliente e são processados conforme necessário. Se não houver mais dados para receber
- * ou se ocorrer um erro de recepção, o cliente é desconectado e removido das estruturas de dados do servidor.
- */
 void Server::reciveNewData(int fd)
 {
 	std::vector<std::string> cmd;
@@ -433,14 +246,6 @@ void Server::reciveNewData(int fd)
 	}
 }
 
-/**
- * Divide uma string recebida em várias linhas.
- * 
- * @param str A string a ser dividida em linhas.
- * @return Um vetor de strings contendo as linhas da string original.
- * 
- * Divide a string recebida em várias linhas com base nos caracteres de quebra de linha '\r' e '\n'.
- */
 std::vector<std::string> Server::splitReceivedBuffer(std::string str)
 {
 	std::vector<std::string> vec;
@@ -456,14 +261,6 @@ std::vector<std::string> Server::splitReceivedBuffer(std::string str)
 	return vec;
 }
 
-/**
- * Divide uma string em tokens separados por espaços.
- * 
- * @param cmd A string a ser dividida em tokens.
- * @return Um vetor de strings contendo os tokens da string original.
- * 
- * Divide a string recebida em tokens com base nos espaços em branco.
- */
 std::vector<std::string> Server::splitCmd(std::string& cmd)
 {
 	std::vector<std::string> vec;
@@ -477,16 +274,6 @@ std::vector<std::string> Server::splitCmd(std::string& cmd)
 	return vec;
 }
 
-/**
- * Verifica se um cliente não está registrado corretamente.
- * 
- * @param fd O descritor de arquivo do cliente.
- * @return Retorna true se o cliente não estiver registrado corretamente, caso contrário, retorna false.
- * 
- * Verifica se o cliente associado ao descritor de arquivo especificado não está registrado corretamente.
- * Um cliente é considerado não registrado se não tiver um apelido (nickname) ou um nome de usuário (username) definidos,
- * ou se estiver marcado como não logado.
- */
 bool Server::notRegistered(int fd)
 {
 	if (!GetClient(fd) || GetClient(fd)->GetNickName().empty() || GetClient(fd)->GetUserName().empty() || GetClient(fd)->GetNickName() == "*"  || !GetClient(fd)->GetLogedIn())
@@ -494,16 +281,6 @@ bool Server::notRegistered(int fd)
 	return true;
 }
 
-/**
- * Analisa e executa um comando recebido do cliente.
- * 
- * @param cmd O comando recebido do cliente.
- * @param fd O descritor de arquivo do cliente.
- * 
- * Analisa o comando recebido do cliente, executa as ações correspondentes com base no comando e no estado do cliente.
- * Os comandos possíveis incluem autenticação, definição de apelido e nome de usuário, ações em canais (como JOIN, TOPIC, MODE, PART),
- * mensagens PRIVMSG, convites (INVITE), e outros. Se o cliente não estiver registrado corretamente, as ações são limitadas.
- */
 void Server::parseExecCmd(std::string &cmd, int fd)
 {
 	if(cmd.empty())
